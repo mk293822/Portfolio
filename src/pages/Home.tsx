d2f0bb9620005e18ba68d89480db2029d7fb1edf
mk293822 <mkt293822@gamil.com>
2026-01-23 23:32:48 +0630

    Fix the profile background error

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -49,7 +49,7 @@ const Home = () => {
 				</div>
 			</header>
 
-			<div className="flex flex-col items-center justify-center h-[calc(90vh+2.5px)] bg-contain bg-no-repeat bg-right flex-1 xl:hero">
+			<div className="hero flex flex-col items-center justify-center h-[calc(90vh+2.5px)] bg-contain bg-no-repeat bg-right flex-1 xl:bg-background">
 				<div className="text-gray-800 text-center flex-1 items-center flex flex-col justify-center">
 					{/* Hero Section */}
 					<div className="text-center max-w-3xl mx-auto">
